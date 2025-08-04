e45f40ec102a662cfde75ac3daa9b1abb0b96a5e
lvr4lvr <alvaro.oteropajares@gmail.com>
2025-08-04 19:18:45 +0200

    Create 2025-08-04_drag-queens.js

---
+++ src/js/data/2025-08-04_drag-queens.js
@@ -0,0 +1,22 @@
+dataSetVersion = "2025-08-04"; // fecha única
+
+dataSet[dataSetVersion] = {
+  options: [
+    {
+      name: "Drag Race All Stars",
+      subtitle: "RuPaul's Drag Race (US)",
+      list: [
+        "Sasha Colby",
+        "Anetra",
+        "Luxx Noir London",
+        "Mistress Isabelle Brooks",
+        "Symone",
+        "Jaida Essence Hall",
+        "Jinkx Monsoon",
+        "Bob the Drag Queen",
+        "Katya",
+        "Trixie Mattel"
+      ]
+    }
+  ]
+};
